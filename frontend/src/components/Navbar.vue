3a1d796c51935ad040aa30d6426adf8f9fa5a6b2
AnthonyClausing <AnthonyClausing@gmail.com>
2019-07-30 19:14:29 -0400

    auth logic moved out of navbar, image view on SP

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -13,60 +13,12 @@
           <b-nav-item v-if="user" :to="`user/${user.name}`" right>{{
             user.name
           }}</b-nav-item>
-          <b-button v-else v-b-modal.modal-1>Login or Register</b-button>
-          <b-modal
-            id="modal-1"
-            centered
-            hide-footer
-            hide-backdrop
-            no-close-on-backdrop
-            ref="authModal"
-          >
-            <b-container fluid>
-              <b-row>
-                <b-col cols="6">
-                  <div id="combinedLeft" class="text-center">
-                    <div class="form-header mb-1">Create Account</div>
-                    <b-form-input
-                      v-model="signup.name"
-                      placeholder="Pick a username"
-                      type="text"
-                      maxlength="18"
-                    ></b-form-input>
-                    <b-form-input
-                      v-model="signup.password"
-                      placeholder="Password"
-                      type="password"
-                    ></b-form-input>
-                    <b-form-input
-                      v-model="signup.email"
-                      placeholder="Optional e-mail address"
-                      type="email"
-                      maxlength="100"
-                    ></b-form-input>
-                    <b-button @click="signupSubmit">Create Account</b-button>
-                  </div>
-                </b-col>
-                <b-col cols="6">
-                  <div id="combinedRight" class="text-center">
-                    <div class="form-header mb-1">Login to HG</div>
-                    <b-form-input
-                      v-model="login.name"
-                      placeholder="Pick a username"
-                      type="text"
-                      maxlength="18"
-                    ></b-form-input>
-                    <b-form-input
-                      v-model="login.password"
-                      placeholder="Password"
-                      type="password"
-                    ></b-form-input>
-                    <b-button @click="loginSubmit">Login</b-button>
-                  </div>
-                </b-col>
-              </b-row>
-            </b-container>
-          </b-modal>
+          <b-button v-else v-b-modal.auth-modal>Login or Register</b-button>
+          <auth-modal />
+          <b-button v-b-modal.post-modal class="variant-danger">
+            MAKE A POST
+          </b-button>
+          <post-modal />
           <!-- <b-nav-item :to="`/user/test1`" right>USER</b-nav-item> -->
         </b-navbar-nav>
       </b-navbar>
@@ -76,39 +28,17 @@
 
 <script>
 import { mapState } from "vuex";
-// import AuthModal from './Auth/AuthModal'
+import AuthModal from "./Modals/AuthModal.vue";
+import PostModal from "./Modals/PostModal.vue";
+
 export default {
   name: "Navbar",
   components: {
-    //AuthModal
-  },
-  data() {
-    return {
-      login: {
-        name: "",
-        password: ""
-      },
-      signup: {
-        name: "",
-        password: "",
-        email: ""
-      }
-    };
+    AuthModal,
+    PostModal
   },
   computed: {
     ...mapState({ user: state => state.user.user })
-  },
-  methods: {
-    signupSubmit() {
-      this.$store.dispatch("user/signup", this.signup).then(() => {
-        this.$refs.authModal.hide();
-      });
-    },
-    loginSubmit() {
-      this.$store.dispatch("user/login", this.login).then(() => {
-        this.$refs.authModal.hide();
-      });
-    }
   }
 };
 </script>
